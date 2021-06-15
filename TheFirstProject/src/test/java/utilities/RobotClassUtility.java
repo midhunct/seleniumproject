812b9113506613a3e4e64e0e85242070e2657953
midhunct <midhunct08@gmail.com>
2021-06-15 12:49:39 +0530

    uploaded file medicalcerificate name changed

---
+++ TheFirstProject/src/test/java/utilities/RobotClassUtility.java
@@ -0,0 +1,39 @@
+package utilities;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.WebDriver;
+
+public class RobotClassUtility {
+	
+	
+	
+	public void robotUploadFile(String filepath) throws AWTException
+	{
+		
+		// creating object of Robot class
+	    Robot rb = new Robot();
+	 
+	    // copying File path to Clipboard
+	    StringSelection str = new StringSelection(filepath);
+	    Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null);
+	 
+	     // press Contol+V for pasting
+	     rb.keyPress(KeyEvent.VK_CONTROL);
+	     rb.keyPress(KeyEvent.VK_V);
+	 
+	    // release Contol+V 
+	    rb.keyRelease(KeyEvent.VK_CONTROL);
+	    rb.keyRelease(KeyEvent.VK_V);
+	 
+	    // for pressing and releasing Enter
+	    rb.keyPress(KeyEvent.VK_ENTER);
+	    rb.keyRelease(KeyEvent.VK_ENTER);
+	}
+	
+
+}
